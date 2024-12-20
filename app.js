3ccd439ffc2a808d241e3f8d63c5668a3fd82a37
leonquinones <lquinonesd@poligran.edu.co>
2024-12-20 15:54:55 -0500

    Create createPerson method in mongo repository

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 import SqlRepository from './repositories/SqlRepository.js'
 import MongoRepository from './repositories/MongoRepository.js';
+import { Collection } from 'mongodb';
 
 
 
@@ -14,23 +15,25 @@ const sqlRepository = new SqlRepository({
   password:process.env.SQL_PASSWORD,
   database:process.env.SQL_DBNAME,
   port:process.env.SQL_DBPORT
+
 })
 
 const mongoRepository = new MongoRepository({
-  host:process.env.MONGO_HOST,
-  user:process.env.MONGO_USER,
-  password:process.env.MONGO_PASSWORD,
-  database:process.env.MONGO_DBNAME,
-  port:process.env.MONGO_DBPORT,
+  connectionString: process.env.MONGO_CONNECTION,
+  database: process.env.MONGO_DBNAME,
   collection:process.env.MONGO_DBCOLLECTION
 })
 
+console.log
 
 app.get('/', async (req, res)=> {
   sqlRepository.createConnection()
+  console.log(mongoRepository.connectionString)
+  mongoRepository.create_connection()
   const person = (await sqlRepository.getPerson({'dni':'2840838509607'}))
   console.log(person)
-  res.send(person)
+  const result = await mongoRepository.createPerson(person)
+  res.send(result)
 })
 
 app.listen(process.env.PORT_API, () => {
--- repositories/MongoRepository.js
+++ repositories/MongoRepository.js
@@ -2,16 +2,15 @@ import { MongoClient } from "mongodb"
 
 export default class MongoRepository {
     constructor(databaseParams) {
+        
         this.databaseParams = databaseParams
-        this.connectionString = 'mongodb:// '+`${this.databaseParams['user']}:${this.databaseParams['password']}
-            //@${this.databaseParams['host']}:${this.databaseParams['port']}/
-            //${this.databaseParams['database']}` 
+        this.connectionString = this.databaseParams['connectionString']//'mongodb://+srv:'+`${this.databaseParams['user']}:${this.databaseParams['password']}@${this.databaseParams['host']}/${this.databaseParams['database']}` 
     }
 
     create_connection(){
-        // this.client = new MongoClient(this.connectionString)
-        // this.connection = this.client.collection(this.databaseParams['collection'])
-        console.log(this.connectionString)
+        this.client = new MongoClient(this.connectionString)
+        const database = this.client.db(this.databaseParams['database'])
+        this.connection = database.collection(this.databaseParams['collection'])
     }
 
     close_connection(){
@@ -19,7 +18,8 @@ export default class MongoRepository {
     }
 
 
-    createPerson(person){
+    async createPerson(person){
+        return this.connection.insertOne(person)
         
     }
 }
